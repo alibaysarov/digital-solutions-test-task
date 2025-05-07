3af990b7561ed99a2969af55d8bcbdcd5eac8e18
alibaysarov <alibajsarov353@gmail.com>
2025-05-07 17:42:38 +0300

    Edit for deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,15 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-    preview:{
+    server: {
         host: '0.0.0.0',
-        port:80
+        port:5173
+    },
+    preview: {
+        host: '0.0.0.0',
+        port: 80
     },
     plugins: [
         react(),
